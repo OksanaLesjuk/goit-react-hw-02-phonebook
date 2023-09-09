c59f6d73d626f1547cff93641dc5453589d7e03a
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2023-09-09 21:09:58 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,20 +1,19 @@
 import Contact from 'components/Contact/Contact';
-import { Component } from 'react';
+
 import { ContactListStyling } from './ContactList.styled';
 
-export default class ContactList extends Component {
-  render() {
-    const { contacts, filteredContacts, deleteContacts } = this.props;
-    return (
-      <ContactListStyling>
-        {(filteredContacts ?? contacts).map(contact => (
-          <Contact
-            deleteContacts={deleteContacts}
-            contact={contact}
-            key={contact.id}
-          />
-        ))}
-      </ContactListStyling>
-    );
-  }
-}
+const ContactList = ({ contacts, filteredContacts, deleteContacts }) => {
+  return (
+    <ContactListStyling>
+      {(filteredContacts ?? contacts).map(contact => (
+        <Contact
+          deleteContacts={deleteContacts}
+          contact={contact}
+          key={contact.id}
+        />
+      ))}
+    </ContactListStyling>
+  );
+};
+
+export default ContactList;
